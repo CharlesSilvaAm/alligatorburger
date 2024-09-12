4290df1a4ba4e68db4409df43dca29997bbde3ca
charles.silva <charles.silva@grupoicts.com.br>
2024-09-12 13:47:40 -0400

    Instala dependências e configura Tailwind; adiciona arquivos CSS e HTML

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './index.html',
+    './styles/**/*.{html,js}',  // Certifique-se de incluir os caminhos corretos
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
